1f3395a17a5b27bfc56a5ce18a538c6bc813c100
khegglu <55990688+khegglu@users.noreply.github.com>
2023-07-25 15:39:58 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -4,7 +4,7 @@
 
 # Filesystem mount warning
 echo "This script will create and format the partitions as follows:"
-echo "/dev/sda1 - 512Mib will be mounted as /boot/efi"
+echo "/dev/sda1 - 512Mib will be mounted as /boot"
 echo "/dev/sda2 - 40GiB will be used as /"
 echo "/dev/sda3 - rest of space will be mounted as /home"
 read -p 'Continue? [y/N]: ' fsok
@@ -26,6 +26,8 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
   1 # partition number 1
     # default - start at beginning of disk 
   +512M # 512 MB boot parttion
+  t # partition type
+  1 # efi system
   n # new partition
   p # primary partition
   2 # partion number 2
@@ -51,8 +53,8 @@ mkfs.fat -F32 /dev/sda1
 # Mount the partitions
 mount /dev/sda2 /mnt
 mount /dev/sda3 /mnt/home
-mkdir -pv /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
+mkdir -pv /mnt/boot
+mount /dev/sda1 /mnt/boot
 
 # Set up time
 timedatectl set-ntp true
