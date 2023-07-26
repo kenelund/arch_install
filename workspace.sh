f05732edcaabcfc92c6f07868e867da7fb41ef29
khegglu <55990688+khegglu@users.noreply.github.com>
2023-07-26 14:11:55 +0200

    Update workspace.sh

--- workspace.sh
+++ workspace.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-core_install="dmenu rxvt-unicode i3-wm i3blocks i3lock i3status dunst feh htop rofi unclutter"
-core_dependencies="acpi alsa-tools alsa-utils android-tools arandr bridge-utils inotify-tools inetutils libnotify lib32-alsa-plugins lib32-libpulse logrotate net-tools pavucontrol pulseaudio-alsa smartmontools tlp xautolock xdotool xfsprogs xprintidle xssstate git"
+base="dmenu rxvt-unicode i3-wm i3blocks i3lock i3status dunst feh htop rofi unclutter xautolock xorg-server xorg-server-utils xorg-twm xorg-xclock xorg-xinit xorg-xprop xorg-xwininfo"
+utility="acpi alsa-tools alsa-utils arandr bridge-utils inotify-tools inetutils libnotify lib32-alsa-plugins lib32-libpulse logrotate net-tools pavucontrol pulseaudio-alsa smartmontools tlp xdotool xfsprogs xprintidle xssstate git unrar unzip zip p7zip gzip"
 applications="cmus deluge-gtk firefox leafpad mpv mupdf neofetch obsidian ranger rclone redshift tmux viewnior"
-extra="adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts ttf-liberation"
-pacman -S --needed $core_install $core_dependencies $applications $extra
+fonts="adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts ttf-liberation"
+extraa="android-tools"
+pacman -Sy
+pacman -S --needed $base $utility $applications $fonts $extra
 
 # arch user repoaitory
 cd /root
@@ -19,21 +21,21 @@ rmdir yay
 
 
 # Setup login management - no display manager
-mkdir /etc/systemd/system/getty@tty1.service.d
+#mkdir /etc/systemd/system/getty@tty1.service.d
 
-cat << EOF > /etc/systemd/system/getty@tty1.service.d/autologin.conf
-[Service]
-ExecStart=
-ExecStart=-/bin/autologin.sh %I
-EOF
+#cat << EOF > /etc/systemd/system/getty@tty1.service.d/autologin.conf
+#[Service]
+#ExecStart=
+#ExecStart=-/bin/autologin.sh %I
+#EOF
 
-cat << EOF > /bin/autologin.sh
-#!/bin/sh
-if [ -f /tmp/autologin ]
-then
-  exec /sbin/agetty --autologin kenneth --noclear $1
-else
-  exec /sbin/agetty --noclear $1
-fi
-EOF
-chmod a+x /bin/autologin.sh
+#cat << EOF > /bin/autologin.sh
+##!/bin/sh
+#if [ -f /tmp/autologin ]
+#then
+#  exec /sbin/agetty --autologin kenneth --noclear $1
+#else
+#  exec /sbin/agetty --noclear $1
+#fi
+#EOF
+#chmod a+x /bin/autologin.sh
